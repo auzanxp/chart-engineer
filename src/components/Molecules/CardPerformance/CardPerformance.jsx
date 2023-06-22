c33a7f4bc22ae0331facffaab1accf198165250e
Auzan <istikhoriauzan@gmail.com>
2023-06-23 01:44:10 +0700

    feat: card performace

--- src/components/Molecules/CardPerformance/CardPerformance.jsx
+++ src/components/Molecules/CardPerformance/CardPerformance.jsx
@@ -5,7 +5,7 @@ const CardPerformance = () => {
   return (
     <div className="mt-2">
       <div className="row">
-        <div className="col-md-4 card_bg">
+        <div className="col-md-4">
           <div className="card-performance__title">
             <p>Gas YTD</p>
           </div>
